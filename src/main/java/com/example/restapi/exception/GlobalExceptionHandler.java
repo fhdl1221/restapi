3d183c6a60f088804248aa432559ccf9c4fca6ae
fhdl1221 <fhdl353212@gmail.com>
2025-12-08 15:17:37 +0900

    Valid, 모든 예외 처리

--- src/main/java/com/example/restapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/restapi/exception/GlobalExceptionHandler.java
@@ -1,9 +1,15 @@
 package com.example.restapi.exception;
 
 import com.example.restapi.dto.response.ApiResponse;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,7 +24,20 @@ public class GlobalExceptionHandler {
     }
 
     // Validation 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(","));
+
+        return ResponseEntity.badRequest()
+                .body(ApiResponse.error("VALIDATION ERROR", message));
+    }
 
 
     // 그 외 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
+        return ResponseEntity.internalServerError().body(ApiResponse.error("INTERNAL_ERROR", "서버 오류가 발생했습니다"));
+    }
 }
