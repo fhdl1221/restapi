58c13f33d0c6d7a5ffb4acaa7ff99e03613c2ea8
fhdl1221 <fhdl353212@gmail.com>
2025-12-09 14:14:14 +0900

    signup, login 기능 구현을 위한 DTO, Service 작성

---
+++ src/main/java/com/example/restapi/dto/request/SignupRequest.java
@@ -0,0 +1,25 @@
+package com.example.restapi.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignupRequest {
+
+    @NotBlank(message = "username은 필수입니다")
+    @Size(min = 4, max = 20, message = "username 길이는 4~20입니다")
+    private String usernmae;
+
+    @NotBlank(message = "password는 필수입니다")
+    private String password;
+
+    @NotBlank(message = "email은 필수입니다")
+    @Email(message = "이메일 형식이 아닙니다")
+    private String email;
+
+    private String name;
+}
